404efb665d0799291da41aef556a234f398b70f7
SAYON DAS <dassayon16@gmail.com>
2025-01-27 22:40:52 +0530

    stack 13

--- stack13.java
+++ stack13.java
@@ -1,14 +1,70 @@
 import java.util.*;
+
+// USING JAVA COLLECTION FRAMEWORK STACK
 public class stack13 {
     public static void main(String[] args) {
-        Stack<Integer> s= new Stack<>();
-        s.push(1);
+        Stack<Integer> s= new Stack<>();  // static ArrayList<Integer> list = new ArrayList<>();
+
+                  //     public static void push(int data) {
+                  //         list.add(data);   // adding from last [1,2,3,4]
+                  //     } 
+
+        s.push(1);   //list.add(data);  
         s.push(2);
         s.push(3); 
             while(!s.isEmpty()){
-                System.out.println((s.peek()));
-                s.pop();
+                System.out.println((s.peek())); //list.get(list.size()-1); 
+                s.pop();  //list.remove(list.size()-1);
             }
     }
    
 }
+// ! USING ARRAYLIST
+
+// public class stack1{
+//     public class stack{
+
+//     // !using arraylist
+//         static ArrayList<Integer> list = new ArrayList<>();
+    
+//     //chacking list is empty or not
+//     public static boolean isEmpty(){
+//         return list.size() == 0; //size 0 means no element
+      
+//     }
+    
+//     // push
+//     public static void push(int data) {
+//         list.add(data);   // adding from last [1,2,3,4]
+//     } 
+    
+//     //pop
+//     public static int pop(){
+//         if(isEmpty()){
+//             return -1;
+//         }
+//         int top = list.get(list.size()-1); //taking the last element [1,2,3.4] -> [4]
+//         list.remove(list.size()-1); //deleting the last element [4]
+//         return top; //returning 4
+//     }
+//     //peek
+//     public static int peek(){
+//         if(isEmpty()){
+//             return -1;
+//         }
+//         return list.get(list.size()-1); //returning the last element
+//     }
+    
+//         public static void main(String[] args) {
+//             Stack s = new Stack();
+//             s.push(1);
+//             s.push(2);
+//             s.push(3);
+//             s.push(4);
+//             while (!s.isEmpty()){
+//                  System.err.println(s.peek());
+//                  s.pop();
+//             }
+//         }
+//     }
+// }
